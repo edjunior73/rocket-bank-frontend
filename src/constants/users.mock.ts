1441e53af491dbd6a6e5ae9da81c416513481aa2
Edmar Siqueira <edsiqueira73@gmail.com>
2021-10-24 04:21:10 -0300

    feat: add users mock data

---
+++ src/constants/users.mock.ts
@@ -0,0 +1,60 @@
+import { User } from 'types'
+
+export const usersMock: User[] = [
+  {
+    id: '1',
+    name: 'José da Silva',
+    birthday: new Date(1990, 1, 1),
+    zipCode: '05311-000',
+    city: 'São Paulo',
+    state: 'SP'
+  },
+  {
+    id: '2',
+    name: 'Marco Antonio Alves',
+    birthday: new Date(2007, 5, 3),
+    zipCode: '05311-000',
+    city: 'Ubutuba',
+    state: 'SP'
+  },
+  {
+    id: '3',
+    name: 'Maria Joaquina',
+    birthday: new Date(2000, 7, 22),
+    zipCode: '05311-000',
+    city: 'Brasília',
+    state: 'SP'
+  },
+  {
+    id: '4',
+    name: 'Mauricio de Souza',
+    birthday: new Date(1991, 1, 1),
+    zipCode: '05311-000',
+    city: 'Niterói',
+    state: 'SP'
+  },
+  {
+    id: '5',
+    name: 'André Silvério Pinto',
+    birthday: new Date(1990, 11, 30),
+    zipCode: '05311-000',
+    city: 'Guarulhos',
+    state: 'SP'
+  },
+  {
+    id: '6',
+    name: 'Eduardo Urbano',
+    birthday: new Date(1977, 5, 8),
+    zipCode: '05311-000',
+    city: 'Rio de Janeiro',
+    state: 'SP'
+  },
+  {
+    id: '7',
+    name: 'Vanessa Alvares Cabral',
+    birthday: new Date(1980, 6, 20),
+    zipCode: '05311-000',
+    city: 'Barueri',
+    state: 'SP'
+  }
+]
