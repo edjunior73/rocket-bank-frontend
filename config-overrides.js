0ae0eff256b18512a1deba7497b046c4c19bd3f3
Edmar Siqueira <edsiqueira73@gmail.com>
2021-10-25 20:10:21 -0300

    chore: add react fast refresh

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,10 @@
 const { addWebpackAlias, override, useBabelRc } = require('customize-cra')
+const { addReactRefresh } = require('customize-cra-react-refresh')
 
 module.exports = override(
   addWebpackAlias({
     '@mui/styled-engine': '@mui/styled-engine-sc'
   }),
+  addReactRefresh(),
   useBabelRc()
 )
