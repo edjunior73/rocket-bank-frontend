49eb5a584ae92e349737d9ddef10e2562389078d
Edmar Siqueira <edsiqueira73@gmail.com>
2021-10-25 23:29:45 -0300

    chore: update deps

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,8 @@
 const { addWebpackAlias, override, useBabelRc } = require('customize-cra')
-const { addReactRefresh } = require('customize-cra-react-refresh')
 
 module.exports = override(
   addWebpackAlias({
     '@mui/styled-engine': '@mui/styled-engine-sc'
   }),
-  addReactRefresh(),
   useBabelRc()
 )
